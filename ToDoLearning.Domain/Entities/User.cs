7c2702f996dbd1cfa17b9b2db7194d08c38355b2
Alex Marden Gomes do Nascimento <alex.nascimento@to-brasil.local>
2019-09-14 12:17:20 -0300

    Creation Entity User

---
+++ ToDoLearning.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ToDoLearning.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+    }   
+}
