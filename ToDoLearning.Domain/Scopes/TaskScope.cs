c328770c02787302f2eb6bba822b024a39995701
Alex Marden Gomes do Nascimento <alex.nascimento@to-brasil.local>
2019-09-14 16:05:59 -0300

    update validations

--- ToDoLearning.Domain/Scopes/TaskScope.cs
+++ ToDoLearning.Domain/Scopes/TaskScope.cs
@@ -1,10 +1,14 @@
-﻿namespace ToDoLearning.Domain.Scopes
-{
-    public class TaskScope
-    {
-        public void ValidTitle()
-        {
+﻿using ToDoLearning.SharedKarnel.Validations;
 
+namespace ToDoLearning.Domain.Scopes
+{
+    public static class TaskScope
+    {
+        public static void ValidTaskTitle( string title)
+        {            
+                AssertionConcern.IsValid(
+                    AssertionConcern.AssertLength(title, 5, 30, "O título da tarefa deve ser maior ou igual a 5 e menor ou igual a 100 caracteres!")
+                );         
         }
     }
 }
--- ToDoLearning.Domain/Scopes/UserScope.cs
+++ ToDoLearning.Domain/Scopes/UserScope.cs
@@ -1,15 +1,21 @@
-﻿namespace ToDoLearning.Domain.Scopes
-{
-    public class UserScope
-    {
-        public void ValidUserName()
-        {
+﻿using ToDoLearning.SharedKarnel.Validations;
 
+namespace ToDoLearning.Domain.Scopes
+{
+    public static class UserScope
+    {
+        public static void ValidUserName(string name)
+        {
+            AssertionConcern.IsValid(
+                AssertionConcern.AssertLength(name, 5, 100, "O nome do usuário deve ser maior ou igual a 5 e menor ou igual a 100 caracteres!")
+            );
         }
 
-        public void ValidEmail()
+        public static void ValidEmail(string email)
         {
-
+            AssertionConcern.IsValid(
+                AssertionConcern.AssertEmailIsValid(email, "O Email não está no formato adequado!")
+            );
         }
     }
 }
