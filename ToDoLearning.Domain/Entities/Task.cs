237758ff6e372eef4bf4e472062076fb6a83e5c2
Breno Ramos <ramosb317@gmail.com>
2019-09-14 11:36:37 -0300

    Criacao da entidade Task do dominio

---
+++ ToDoLearning.Domain/Entities/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ToDoLearning.Domain.Shared;
+
+namespace ToDoLearning.Domain.Entities
+{
+    public class Task : Entity
+    {
+        public Task(string title)
+        {
+            Title = title;
+            CreateDate = DateTime.Now;
+        }
+        
+        public string Title { get; private set; }
+        public DateTime CreateDate { get; private set; }
+    }
+}
