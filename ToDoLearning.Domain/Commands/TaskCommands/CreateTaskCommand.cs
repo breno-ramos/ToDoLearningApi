53345c5fe0df1a6730f0c9744ab8bf9e52baac48
Breno Ramos <ramosb317@hotmail.com>
2019-10-15 20:43:25 -0300

    Creation of application service project and commands of the task

---
+++ ToDoLearning.Domain/Commands/TaskCommands/CreateTaskCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ToDoLearning.Domain.Commands.TaskCommands
+{
+    public class CreateTaskCommand
+    {
+        public CreateTaskCommand(Guid userId, string title)
+        {
+            UserId = userId;
+            Title = title;
+        }
+
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+    }
+}
