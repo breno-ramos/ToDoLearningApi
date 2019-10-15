2cdc407df824d37b81ee3fc43788114448604b99
Alex Marden Gomes do Nascimento <alex.nascimento@to-brasil.local>
2019-10-15 20:52:34 -0300

    creation command of the users and application services interfaces

---
+++ ToDoLearning.Domain/Commands/UserCommands/UpdateUserCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoLearning.Domain.Commands.UserCommands
+{
+    public class UpdateUserCommand
+    {
+        public UpdateUserCommand(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
