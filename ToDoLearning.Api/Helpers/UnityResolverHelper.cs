5825d15a86683d06bc44f414e5b78aba080c5c7d
Breno Ramos <ramosb317@hotmail.com>
2019-10-15 23:07:49 -0300

    Creation of the api project and project for dependency injection

---
+++ ToDoLearning.Api/Helpers/UnityResolverHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Unity;
+
+
+namespace ToDoLearning.Api.Helpers
+{
+    public class UnityResolverHelper : IDependencyResolver
+    {
+        protected IUnityContainer _container;
+        public UnityResolverHelper(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            _container = container;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = _container.CreateChildContainer();
+            return new UnityResolverHelper(child);
+        }
+
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return _container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return _container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }        
+    }
+}
---
+++ ToDoLearning.CrossCutting/DependencyRegister.cs
@@ -0,0 +1,22 @@
+﻿using Unity;
+using Unity.Lifetime;
+using ToDoLearning.Domain.Repositories;
+using ToDoLearning.InfraEstructure.Repositories;
+using ToDoLearning.Domain.Services;
+using ToDoLearning.ApplicationService;
+using ToDoLearning.InfraEstructure.DataContext;
+
+namespace ToDoLearning.CrossCutting
+{
+    public static class DependencyRegister
+    {
+        public static void Register(UnityContainer container)
+        {
+            container.RegisterType<AppDataContext, AppDataContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<ITaskRepository, TaskRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<ITaskApplicationService, TaskApplicationService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserApplicationService, UserApplicationService>(new HierarchicalLifetimeManager());            
+        }
+    }
+}
